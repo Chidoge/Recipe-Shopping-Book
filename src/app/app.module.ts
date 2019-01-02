64f713f9bc055f20de4f3697d96a09f9c2b25551
unknown <lincoln.choy1997@gmail.com>
2019-01-02 21:11:00 +1300

    Added shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { RecipesModule } from './recipes/recipes.modules';
+import { ShoppingListModule } from './shopping-list/shopping-list.module'
 import { RecipesRoutingModule } from './recipes/recipes-routing.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './shared/dropdown.directive';
+
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+
 import { DataStorageService } from './shared/data-storage.service';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
@@ -25,17 +25,16 @@ import { AuthGuardService } from './auth/auth-guard.service';
 	declarations: [
 		AppComponent,
 		HeaderComponent,
-		ShoppingListComponent,
-		ShoppingEditComponent,
 		SignupComponent,
 		SigninComponent
 	],
 	imports: [
 		BrowserModule,
 		RecipesModule,
+		ShoppingListModule,
+		SharedModule,
 		AppRoutingModule,
-		FormsModule,
-		HttpModule,
+		HttpModule
 		
 	],
 	providers: [ RecipeService, DataStorageService, ShoppingListService, AuthService, AuthGuardService ],
--- src/app/recipes/recipes.modules.ts
+++ src/app/recipes/recipes.modules.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RecipesRoutingModule } from './recipes-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 import { RecipesComponent } from './recipes.component';
-import { DropdownDirective } from '../shared/dropdown.directive';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component'; 
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
@@ -19,13 +19,13 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 		RecipeStartComponent,
 		RecipeDetailComponent,
 		RecipeItemComponent,
-		RecipeEditComponent,
-		DropdownDirective
+		RecipeEditComponent
 	],
 	imports : [
 		CommonModule,
 		ReactiveFormsModule,
-		RecipesRoutingModule
+		RecipesRoutingModule,
+		SharedModule
 	]
 })
 
